95dd81ad7335947a1abfc2859bc27f023d7ebf72
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-05-07 15:41:10 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billitsite::Application.config.secret_token = '34e3d5518ee3faab79d9f3b19bd68f1e4982c1bf49d51de9febef4cf8b43eae4d1fb29930ffa93b5f31f5dad72df8aa34bb06e8eba11b1d7ff72f62af4140bb2'
